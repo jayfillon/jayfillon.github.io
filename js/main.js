41e88dbd6aa50079ad91a1edbb253f7abadd4f45
Jericho FIllon <jerichofilloncoding@gmail.com>
2020-04-08 21:47:14 -0700

    Deployment to GH-pages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*global $, jQuery, alert*/
+$(document).ready(function() {
+
+var typed = {
+    strings: ['Jericho Fillon', 'A Developer', 'A Designer', 'A Photographer', 'Jericho Fillon'],
+    typeSpeed: 50
+};
+
+var typed = new Typed('.typed', typed);
+
+});
